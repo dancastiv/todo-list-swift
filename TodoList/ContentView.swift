63041d9806e9861b2f321e55902e3f28e7d73833
Danny Castilla <dancastiv@gmail.com>
2024-04-11 14:13:01 +0100

    you can add tasks to todo list

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toggledOn = false
+    @State private var userInput = ""
+    @State private var todoList = [String]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("todo")
+                .resizable()
+                .frame(width: 90, height: 90)
+                .scaledToFit()
+            Text("Todos")
+                .font(.largeTitle)
+            Text("Toggle them off")
+            List {
+                TextField("Add task", text: $userInput)
+                Button("Submit") {
+                    guard !userInput.isEmpty else {return}
+                    todoList.append(userInput)
+                    userInput = ""
+                }
+                Section {
+                    ForEach(todoList, id: \.self) {task in
+                    Toggle(task, isOn: $toggledOn)
+                    }
+                }
+            }            
         }
         .padding()
     }
